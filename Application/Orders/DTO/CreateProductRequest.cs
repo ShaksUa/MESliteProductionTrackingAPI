7b3ee223c9be0235bd2d34cab090eef28a03486c
Serhii <sergiylinskiy@gmail.com>
2026-07-17 10:57:23 +0300

    Route Query JSON-->DTO DTO data "abc"-->int --> 400 Bad Request

---
+++ Application/Orders/DTO/CreateProductRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Orders.DTO
+{
+    public class CreateProductRequest
+    {
+        public string Name { get; set; }
+        public string? Descr { get; set; }
+
+    }
+}
