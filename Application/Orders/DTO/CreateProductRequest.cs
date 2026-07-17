961ddd1327aae85177dd46cc035ab32ce4b91c02
Serhii <sergiylinskiy@gmail.com>
2026-07-17 10:57:23 +0300

    Route Query JSON-->DTO DTO data "abc"-->int --> 400 Bad Request

---
+++ Application/Orders/DTO/CreateProductRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Orders.DTO
+{
+    public class CreateProductRequest
+    {
+        public string name { get; set; }
+        public string? descr { get; set; }
+
+    }
+}
--- MESliteProductionTrackingAPI/Controllers/TestController.cs
+++ MESliteProductionTrackingAPI/Controllers/TestController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Application.Orders.DTO;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MESliteProductionTrackingAPI.Controllers
 {
@@ -16,6 +17,20 @@ namespace MESliteProductionTrackingAPI.Controllers
         public IActionResult Add(int a, int b)
         {
             return Ok (a + b);
+        }
+
+        [HttpGet("product/{id}")]
+        public IActionResult GetProductById (string category, int page, int pagesize)
+        {
+            
+            return Ok("category: " + category + " page: " + page + " pagesize: " + pagesize);
+        }
+
+        [HttpPost("product")]
+        public IActionResult CreateProduct (CreateProductRequest createProductRequest)
+        {
+            return Ok(createProductRequest);
+                //("id: " + id + " Name: " + name + " Description: " + descr + " Create Time:" +creatTime);
         }
     }
 }
